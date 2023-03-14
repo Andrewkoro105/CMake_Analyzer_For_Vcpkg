206541398ce80c1147665c366de853dfe92674fc
Andrewkoro105 <Andrewkoro105@gmail.com>
2023-03-14 20:30:00 +0300

    added skript for var in windows

---
+++ install_var.cmd
@@ -0,0 +1,3 @@
+setx CMAKE_TOOLCHAIN_FILE C:\dev\vcpkg\scripts\buildsystems\vcpkg.cmake
+setx VCPKG_DEFAULT_TARGET_TRIPLET x64-mingw-static
+echo added VCPKG_TARGET_TRIPLET in cmake settings and reboot
